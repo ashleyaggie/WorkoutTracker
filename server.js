19d1b466675a6ed573c0f83a0bcca9afee78c1c2
Ashley <ashleyaggie@aol.com>
2021-06-10 03:36:18 -0500

    Corrected url and added UnifiedTopology.

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true,
-    useFindAndModify:false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 app.use(require("./controllers/"));

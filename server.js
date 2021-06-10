bb88c11d0d4444b2e8aa96d3fb763c74790c2f18
Ashley <ashleyaggie@aol.com>
2021-06-10 04:23:55 -0500

    Cleaning up.

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 app.use(require("./controllers/"));
